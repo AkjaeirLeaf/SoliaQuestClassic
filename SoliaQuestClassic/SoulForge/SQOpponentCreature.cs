57d87441e3c2fafd8838d4e5357d4d82cedf37d5
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-06-23 00:33:18 -0400

    Added Dummy

--- SoliaQuestClassic/SoulForge/SQOpponentCreature.cs
+++ SoliaQuestClassic/SoulForge/SQOpponentCreature.cs
@@ -103,54 +103,100 @@ namespace SoliaQuestClassic.SoulForge
             FullHeal();
             LearnDefaultAbilities();
             SetupInventory();
+            AddTag("isTrainingDummy", false);
+        }
+
+        public SQOpponentCreature()
+        {
+            if(!SQWorld.SQWorldSpeciesList.TryGetValue("dummy1", out m_species))
+            {
+                m_species = new SoulForge.Species.TrainingDummy();
+            }
+
+            m_speciesInternal = m_species.InternalName;
+
+            stat_Health  = m_species.BaseStatHealth;
+            stat_Defense = m_species.BaseStatDefense;
+            stat_Attack  = m_species.BaseStatAttack;
+            stat_Stamina = m_species.BaseStatStamina;
+            stat_Evade   = m_species.BaseStatEvade;
+            stat_Control = m_species.BaseStatControl;
+
+            stat_advanceHealth  = stat_Health;
+            stat_advanceDefense = stat_Defense;
+            stat_advanceAttack  = stat_Attack;
+            stat_advanceStamina = stat_Stamina;
+            stat_advanceEvade   = stat_Evade;
+            stat_advanceControl = stat_Control;
+
+            //TODO add stat mods and color mods, implement stat changes fetching.
+
+            statMod = "ordinary";
+            initialStatMod = statMod; upgraded = false;
+            UpdateAllStatRanks();
+
+            //set full stats
+            FullHeal();
+            LearnDefaultAbilities();
+            SetupInventory();
+            AddTag("isTrainingDummy", true);
         }
 
         private int turn = 0;
         public string GetAbilityUse(SQCreature target)
         {
-            //Basic AI for enemy, pick which ability to use.
+            object isDummy = false;
+            TryGetTag("isTrainingDummy", out isDummy);
+            if (!(bool)isDummy)
+            {
+                //Basic AI for enemy, pick which ability to use.
 
-            if(turn == 0)
-            {
-                int ind = -1;
-                ind = FindAbilityType(SQAbilityPurpose.APPLY_DAMAGE_EFFECT);
-                if (ind != -1) { return Abilities[ind].InternalName; }
-                ind = FindAbilityType(SQAbilityPurpose.STAT_HIT);
-                if (ind != -1) { return Abilities[ind].InternalName; }
-                ind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
-                if (ind != -1) { return Abilities[ind].InternalName; }
-            }
-            //If under 50% health, will begin using defensive measures
-            else if(dynamic_Health < stat_advanceHealth * 0.5)
-            {
-                int healind = -1;
-                healind = FindAbilityType(SQAbilityPurpose.HEAL_ONLY);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-                healind = FindAbilityType(SQAbilityPurpose.HEAL_DAMAGE);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-                healind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-                healind = FindAbilityType(SQAbilityPurpose.DAMAGE_REFLECT);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-            }
-            //if under 20% health, will begin using very defensive measures
-            else if (dynamic_Health < stat_advanceHealth * 0.2)
-            {
-                int healind = -1;
-                if (Kirali.Framework.Random.Int(0, 100) < 33)
+                if (turn == 0)
                 {
-                    healind = FindAbilityType(SQAbilityPurpose.USE_ITEM);
+                    int ind = -1;
+                    ind = FindAbilityType(SQAbilityPurpose.APPLY_DAMAGE_EFFECT);
+                    if (ind != -1) { return Abilities[ind].InternalName; }
+                    ind = FindAbilityType(SQAbilityPurpose.STAT_HIT);
+                    if (ind != -1) { return Abilities[ind].InternalName; }
+                    ind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
+                    if (ind != -1) { return Abilities[ind].InternalName; }
+                }
+                //If under 50% health, will begin using defensive measures
+                else if (dynamic_Health < stat_advanceHealth * 0.5)
+                {
+                    int healind = -1;
+                    healind = FindAbilityType(SQAbilityPurpose.HEAL_ONLY);
                     if (healind != -1) { return Abilities[healind].InternalName; }
-                } //using up all the heals would be a bit much.....
-                healind = FindAbilityType(SQAbilityPurpose.HEAL_ONLY);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-                healind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
-                if (healind != -1) { return Abilities[healind].InternalName; }
-                healind = FindAbilityType(SQAbilityPurpose.DAMAGE_REFLECT);
-                if (healind != -1) { return Abilities[healind].InternalName; }
+                    healind = FindAbilityType(SQAbilityPurpose.HEAL_DAMAGE);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                    healind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                    healind = FindAbilityType(SQAbilityPurpose.DAMAGE_REFLECT);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                }
+                //if under 20% health, will begin using very defensive measures
+                else if (dynamic_Health < stat_advanceHealth * 0.2)
+                {
+                    int healind = -1;
+                    if (Kirali.Framework.Random.Int(0, 100) < 33)
+                    {
+                        healind = FindAbilityType(SQAbilityPurpose.USE_ITEM);
+                        if (healind != -1) { return Abilities[healind].InternalName; }
+                    } //using up all the heals would be a bit much.....
+                    healind = FindAbilityType(SQAbilityPurpose.HEAL_ONLY);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                    healind = FindAbilityType(SQAbilityPurpose.STAT_BOOST);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                    healind = FindAbilityType(SQAbilityPurpose.DAMAGE_REFLECT);
+                    if (healind != -1) { return Abilities[healind].InternalName; }
+                }
+                if (Abilities.Length > 0) { return Abilities[0].InternalName; }
+                return "";
             }
-            if(Abilities.Length > 0) { return Abilities[0].InternalName; }
-            return "";
+            else
+            {
+                return "blankStare";
+            }
         }
 
         private int FindAbilityType(SQAbilityPurpose purpose)
