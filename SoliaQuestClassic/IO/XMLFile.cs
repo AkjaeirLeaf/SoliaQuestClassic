4b8c686b14bafaa15832c857a46413a48f9d866d
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-11-29 10:56:36 -0500

    Begin implement XML/COLLADA Readers

---
+++ SoliaQuestClassic/IO/XMLFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.IO
+{
+    public class XMLFile
+    {
+        public string version  = "1.0";
+        public string encoding = "utf-8";
+
+    }
+}
---
+++ SoliaQuestClassic/IO/XMLStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.IO
+{
+    public struct XMLStructure
+    {
+        public string StructureTag;
+        public string[] Parameters;
+        public bool IsOpenClose; // Open Close: <meow cat="12"></meow> Not open close: <meow cat="12"/>
+        public bool IsContainer; // Container has no contents, only children.
+
+        public XMLStructure[] Children;
+        public object Contents;
+    }
+
+    public enum XMLStructureContentType
+    {
+        UNKNOWN,
+        STRING,
+        INTEGER,
+        DATETIME,
+        ARRAY_UNKNOWN,
+        ARRAY_STRING,
+        ARRAY_INTEGER,
+        ARRAY_DATETIME
+    }
+}
