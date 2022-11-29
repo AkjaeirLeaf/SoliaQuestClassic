8e1e8a23f0b00d3114115963b85ae1a2fb461ff8
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-11-29 10:40:52 -0500

    Two effects added: Hydrate and Thorns

---
+++ SoliaQuestClassic/SoulForge/Effects/HydrateEffect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.SoulForge.Effects
+{
+    public class HydrateEffect : SQEffect
+    {
+        public HydrateEffect()
+        {
+            ModifyEffectReference("Hydrated", "hydrateEffect");
+            tooltip = "You're fully saturated!";
+            stackMax = 1;
+            doShowEffect = true;
+            m_timeLeft = -1;
+        }
+
+        public override int EffectEvent_Activated(SQCreature effected)
+        {
+            effected.AddDynamicTyping("water");
+
+            return base.EffectEvent_Activated(effected);
+        }
+
+        public override int EffectEvent_RemoveEffect(SQCreature effected)
+        {
+            effected.RemoveDynamicTyping("water");
+            effected.DoModifyDefense(effected.DynamicDefense + effected.DefaultDefense / 2);
+            effected.DoModifyEvade(effected.DynamicEvade * 0.82);
+
+            return base.EffectEvent_RemoveEffect(effected);
+        }
+
+    }
+}
---
+++ SoliaQuestClassic/SoulForge/Effects/ThornsEffect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.SoulForge.Effects
+{
+    public class ThornsEffect : SQEffect
+    {
+        public ThornsEffect()
+        {
+            ModifyEffectReference("Thorns", "thornsEffect");
+            tooltip = "Your body is covered in spiky thorns, attacks on you deal damage back to the attacker.";
+            stackMax = 1;
+            doShowEffect = true;
+            m_timeLeft = -1;
+        }
+
+        public override int EffectEvent_DoAbilityUsedOn(SQAbility ability, SQCreature effected, SQCreature sender)
+        {
+            if(ability.Category == SQAbilityCategory.PHYSICAL) //true is for test only remove after
+            {
+                string[] types = new string[] { "plant", "toxic" };
+                SQType[] trueTypes = new SQType[types.Length];
+                for (int t = 0; t < types.Length; t++)
+                {
+                    SQWorld.SQWorldTypeList.TryGetValue(types[t], out trueTypes[t]);
+                }
+
+                SQAbilityInfo info = new SQAbilityInfo();
+                info.abilityDisplay = "Thorned";
+                info.abilityType = trueTypes;
+                info.targetDodges = false;
+                info.doDamageTarget = 13; //flat
+
+                effected.DoDamage(info);
+            }
+
+            
+            return base.EffectEvent_DoAbilityUsedOn(ability, effected, sender);
+        }
+    }
+}
