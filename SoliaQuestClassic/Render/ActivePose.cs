0dc34ab79821a793c88a7628a267aa386e5a40d0
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-12-25 18:30:44 -0500

    Armatures and bones and animation oh my!

---
+++ SoliaQuestClassic/Render/ActivePose.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SoliaQuestClassic.IO;
+using Kirali.MathR;
+
+namespace SoliaQuestClassic.Render
+{
+    public class ActivePose
+    {
+
+
+    }
+
+    public class ActiveBone
+    {
+        private Armature_bone Basis_Define;
+        public Armature_bone DefaultBone { get { return Basis_Define; } }
+
+        private Vector3 m_Axis_Thet; private Vector3 m_Axis_Thet_default;
+        private Vector3 m_Axis_Phie; private Vector3 m_Axis_Phie_default;
+        private Vector3 m_Axis_Radi; private Vector3 m_Axis_Radi_default;
+
+        private Vector3 m_HeadPos; private Vector3 m_HeadPos_default; 
+        private Vector3 m_TailPos; private Vector3 m_TailPos_default;
+
+        public Vector3 Axis_Thet { get { return m_Axis_Thet; } }
+        public Vector3 Axis_Phie { get { return m_Axis_Phie; } }
+        public Vector3 Axis_Radi { get { return m_Axis_Radi; } }
+
+        public Vector3 Head_Position { get { return m_HeadPos; } }
+        public Vector3 Tail_Position { get { return m_TailPos; } }
+        public Vector3 Head_Relative { get { return m_HeadPos - m_TailPos; } }
+        public Vector3 Tail_Relative { get { return m_TailPos - m_HeadPos; } }
+
+        //
+        //
+        //
+
+
+
+        public void Hard_Reset_Pose()
+        {
+            m_Axis_Thet = m_Axis_Thet_default;
+            m_Axis_Phie = m_Axis_Phie_default;
+            m_Axis_Radi = m_Axis_Radi_default;
+
+            m_HeadPos = m_HeadPos_default;
+            m_TailPos = m_TailPos_default;
+        }
+
+        public void Mesh_Deform(Vector3 Axis, double radians)
+        {
+            // DEFORM MESH AND JUMP INTO THE SEA
+            Vector3 axi_normal = Axis.SafeNormalize();
+            for (int px = 0; px < obj_.PosedVertices.Length; px++)
+            {
+                PointInfluence PI_L = obj_.Point_Armature_Array[px];
+                if(PI_L.JOINT != null)
+                {
+                    for (int pl = 0; pl < PI_L.JOINT.Length; pl++)
+                    {
+                        if (PI_L.JOINT[pl] == jointID)
+                        {
+                            double weight = obj_.BoneInfluenceRef[PI_L.WEIGHT[pl]];
+                            Matrix deform = Matrix.RotationU(axi_normal, radians * weight);
+                            obj_.PosedVertices[px] = m_HeadPos + ((obj_.PosedVertices[px] - m_HeadPos).ToMatrix().Flip() * deform).ToVector3();
+                            //break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PoseBone_RotateThet(double radians)
+        {
+            Matrix mat = Matrix.RotationU(m_Axis_Thet.SafeNormalize(), radians);
+            m_Axis_Radi = (m_Axis_Radi.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+            m_Axis_Phie = (m_Axis_Phie.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+
+            m_TailPos = m_HeadPos + (Tail_Relative.ToMatrix().Flip() * mat).ToVector3();
+
+            Mesh_Deform(m_Axis_Thet, radians);  
+        }
+        public void PoseBone_RotatePhie(double radians)
+        {
+            Matrix mat = Matrix.RotationU(m_Axis_Phie.SafeNormalize(), radians);
+            m_Axis_Thet = (m_Axis_Thet.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+            m_Axis_Radi = (m_Axis_Radi.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+
+            m_TailPos = m_HeadPos + (Tail_Relative.ToMatrix().Flip() * mat).ToVector3();
+
+            Mesh_Deform(m_Axis_Phie, radians);
+        }
+        public void PoseBone_RotateRadi(double radians)
+        {
+            Matrix mat = Matrix.RotationU(m_Axis_Radi.SafeNormalize(), radians);
+            m_Axis_Thet = (m_Axis_Thet.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+            m_Axis_Phie = (m_Axis_Phie.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+
+            m_TailPos = m_HeadPos + (Tail_Relative.ToMatrix().Flip() * mat).ToVector3();
+
+            Mesh_Deform(m_Axis_Radi, radians);
+        }
+        public void PoseBone_RotateExternalSpecify(Vector3 axis, double radians, Vector3 center_absolute)
+        {
+            Matrix mat = Matrix.RotationU(axis.SafeNormalize(), radians);
+            m_Axis_Thet = (m_Axis_Thet.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+            m_Axis_Phie = (m_Axis_Phie.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+            m_Axis_Radi = (m_Axis_Radi.SafeNormalize().ToMatrix().Flip() * mat).ToVector3();
+
+            m_HeadPos = center_absolute + ((m_HeadPos - center_absolute).ToMatrix().Flip() * mat).ToVector3();
+            m_TailPos = center_absolute + ((m_TailPos - center_absolute).ToMatrix().Flip() * mat).ToVector3();
+        }
+
+        // Order should always be ^v  <> roll (theta phi radial)
+        private PoseableObject obj_;
+        private int jointID = 0;
+
+        public ActiveBone(Armature_bone basis, int joint, PoseableObject obj_ref)
+        {
+            obj_ = obj_ref;
+            jointID = joint;
+
+            //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            if (false) { basis = FillBoneValues(basis, true, obj_ref); }
+
+
+            Basis_Define = basis;
+            m_Axis_Thet = new Vector3(Basis_Define.Axis_T);
+            m_Axis_Phie = new Vector3(Basis_Define.Axis_P);
+            m_Axis_Radi = new Vector3(Basis_Define.Axis_R);
+
+            m_Axis_Thet_default = new Vector3(m_Axis_Thet);
+            m_Axis_Phie_default = new Vector3(m_Axis_Phie);
+            m_Axis_Radi_default = new Vector3(m_Axis_Radi);
+
+            m_HeadPos = new Vector3(Basis_Define.Head);
+            m_TailPos = new Vector3(Basis_Define.Tail);
+
+            m_HeadPos_default = new Vector3(m_HeadPos);
+            m_TailPos_default = new Vector3(m_TailPos);
+
+        }
+
+        private static Armature_bone FillBoneValues(Armature_bone b, bool fixSender = false, PoseableObject sender = null)
+        {
+            if (!b.axi_res)
+            {
+                b.Head = new double[] { 0, 0, 0 };
+                b.Tail = new double[] { 0, 0, 1 };
+                b.Axis_T = new double[] { 1, 0, 0 };
+                b.Axis_P = new double[] { 0, 1, 0 };
+                b.Axis_R = new double[] { 0, 0, 1 };
+
+                if (fixSender)
+                {
+                    for (int ix = 0; ix < sender.LinkObject.Armature_Bones.Length; ix++)
+                    {
+                        if (sender.LinkObject.Armature_Bones[ix].Name == b.Name)
+                        {
+                            sender.LinkObject.Armature_Bones[ix] = b;
+                            break;
+                        }
+                    }
+                }
+            }
+            b.axi_res = true;
+
+            return b;
+        }
+
+    }
+}
---
+++ SoliaQuestClassic/Render/PoseableObject.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SoliaQuestClassic.IO;
+
+using Kirali.MathR;
+using Kirali.Light;
+using Kirali.Environment.Render.Primatives;
+
+namespace SoliaQuestClassic.Render
+{
+    public class PoseableObject
+    {
+        private Object3D ref_linkObjMesh;
+        public Object3D LinkObject { get { return ref_linkObjMesh; } set { ref_linkObjMesh = value; } }
+        private Vector3[] m_posedVertices_keepVAO;
+        public Vector3[] PosedVertices { get { return m_posedVertices_keepVAO; } set { m_posedVertices_keepVAO = value; } }
+        public PointInfluence[] Point_Armature_Array { get { return ref_linkObjMesh.Point_Armature_Array; } }
+        public double[] BoneInfluenceRef { get { return ref_linkObjMesh.BoneInfluenceRef; } }
+
+        public ActiveBone[] boneGroup = new ActiveBone[0];
+
+        public PoseableObject(Object3D linkedObject)
+        {
+            ref_linkObjMesh = linkedObject;
+
+            // Create posable bones
+            boneGroup = new ActiveBone[linkedObject.Armature_Bones.Length];
+            for(int ix = 0; ix < linkedObject.Armature_Bones.Length; ix++)
+            {
+                boneGroup[ix] = new ActiveBone(linkedObject.Armature_Bones[ix], ix, this);
+            }
+
+            // Copy vao
+            ResetPose();
+        }
+
+        public void ResetPose()
+        {
+            m_posedVertices_keepVAO = new Vector3[ref_linkObjMesh.PointData.Length];
+            for (int ix = 0; ix < boneGroup.Length; ix++) { boneGroup[ix].Hard_Reset_Pose(); }
+            for (int ix = 0; ix < m_posedVertices_keepVAO.Length; ix++) { m_posedVertices_keepVAO[ix] = new Vector3(ref_linkObjMesh.PointData[ix]); }
+        }
+
+        public Triangle3D GetPosedTriangle(int index)
+        {
+            if (index < ref_linkObjMesh.TriangleCount)
+            {
+                Vector3[] tpoints = new Vector3[3];
+                tpoints[0] = m_posedVertices_keepVAO[ref_linkObjMesh.PointRef[index * 3 + 0]];
+                tpoints[1] = m_posedVertices_keepVAO[ref_linkObjMesh.PointRef[index * 3 + 1]];
+                tpoints[2] = m_posedVertices_keepVAO[ref_linkObjMesh.PointRef[index * 3 + 2]];
+
+
+                Triangle3D tri = new Triangle3D(tpoints);
+                tri.TextureLink[0] = ref_linkObjMesh.UV_REF[index * 3 + 0];
+                tri.TextureLink[1] = ref_linkObjMesh.UV_REF[index * 3 + 1];
+                tri.TextureLink[2] = ref_linkObjMesh.UV_REF[index * 3 + 2];
+                tri.UV_Link[0] = ref_linkObjMesh.UV_MAP[ref_linkObjMesh.UV_REF[index * 3 + 0]];
+                tri.UV_Link[1] = ref_linkObjMesh.UV_MAP[ref_linkObjMesh.UV_REF[index * 3 + 1]];
+                tri.UV_Link[2] = ref_linkObjMesh.UV_MAP[ref_linkObjMesh.UV_REF[index * 3 + 2]];
+
+
+
+                return tri;
+            }
+            throw new IndexOutOfRangeException("Referenced triangle does not exist in the mesh. Use TriangleCount to check size of mesh.");
+        }
+
+        public Triangle3D[] AllObjectTriangles(Kirali.Light.Camera camera)
+        {
+            int TriangleCount = ref_linkObjMesh.TriangleCount;
+            double limiter = 0.1;
+            int tc = 0;
+            Triangle3D[] op = new Triangle3D[TriangleCount];
+            bool[] doAdd = Kirali.Framework.ArrayHandler.setAll(false, TriangleCount);
+            for (int l = 0; l < TriangleCount; l++)
+            {
+                op[l] = GetPosedTriangle(l);
+                if (Kirali.MathR.Vector3.Dot(op[l].Normal, op[l].Middle - camera.position) < limiter) { doAdd[l] = true; tc++; }
+            }
+            Triangle3D[] adds = new Triangle3D[tc];
+            tc = 0;
+            for (int l = 0; l < op.Length; l++)
+            {
+                if (doAdd[l]) { adds[tc] = op[l]; tc++; }
+            }
+            return adds;
+        }
+
+        public void Render(Kirali.Light.Camera MainCamera, int useTextureSlot, Vector3 LightSource, KColor4 LightColor)
+        {
+            ref_linkObjMesh.Render(MainCamera, useTextureSlot, LightSource, LightColor, this);
+        }
+    }
+}
