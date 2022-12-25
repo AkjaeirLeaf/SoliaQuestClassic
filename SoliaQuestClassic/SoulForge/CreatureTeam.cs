5fa163cb076264eed64f9f5dcd91a713e4574536
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-12-25 18:32:06 -0500

    Added this for future purposes

---
+++ SoliaQuestClassic/SoulForge/CreatureTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.SoulForge
+{
+    public class CreatureTeam
+    {
+        public SQCreature[] Members = new SQCreature[0];
+        public bool[] Active = new bool[0];
+
+    }
+}
