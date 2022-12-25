bbb37895108fa80b611f7dfcc641e303ba3c675c
AkjaeirLeaf <akjaeirleafk@gmail.com>
2022-12-25 18:31:26 -0500

    snippet for storing bone and weight pointers, 1:1 with the VAO

---
+++ SoliaQuestClassic/IO/PointInfluence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliaQuestClassic.IO
+{
+    public struct PointInfluence
+    {
+        public int[] JOINT;
+        public int[] WEIGHT;
+    }
+}
